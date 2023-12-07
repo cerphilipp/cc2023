8740f1311f7497a88a9c6039add1a2ffc9dbb0cd
Anna Holzner <anna.holzner@stud.th-rosenheim.de>
2023-12-07 14:04:33 +0100

    merge

--- src/main/java/cc2023/teamrandom/ccservice/controller/TroetController.java
+++ src/main/java/cc2023/teamrandom/ccservice/controller/TroetController.java
@@ -16,7 +16,6 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import ch.qos.logback.core.status.Status;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +38,7 @@ public class TroetController {
     public GetHomeService service = new GetHomeService();
     private TroetListService troetListService;
 
-    public GetHomeService service = new GetHomeService();
+
 
     Gson gson;
 
@@ -63,11 +62,11 @@ public class TroetController {
 
 //    @GetMapping("api/home/troets")
 //    public ResponseEntity<String> list(){
-//        return new ResponseEntity<>("Hello World", HttpStatus.OK);
-//    }
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+   }
 
     @RequestMapping(value = "/gethome", method = GET)
-    public ResponseEntity<Status[]> getHome () {
+    public ResponseEntity<Status[]> getHome() {
         Status[] response = new Status[0];
         try {
             response = new ObjectMapper().reader().forType(Status[].class)
@@ -96,13 +95,15 @@ public class TroetController {
             return new ResponseEntity<>("505", HttpStatus.BAD_REQUEST);
         }
     }
-
+/*
     @RequestMapping(value = "/gethome", method = GET)
     public ResponseEntity<Status[]> getHome () {
         Status[] response = gson.fromJson(service.getHome(), Status[].class);
         return new ResponseEntity<>(response, HttpStatus.OK);
 
     }
+    */
+
 
     @RequestMapping(value = "/api/home/troets/reblogged")
     public ResponseEntity<String> reblogged(@RequestParam(name="troeter", required = false) String troeter){
@@ -113,6 +114,7 @@ public class TroetController {
         ArrayList<Status> resultAsArrayList = new ArrayList<>();
         if(troeter==null) {
             for (Status status : entireStatus) {
+
                 if (status.isReblogged()) resultAsArrayList.add(status);
             }
         } else {
