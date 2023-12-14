340bb16dbee1ccfdde2bec1c1978ee91f273a65d
studwalllu3849 <lukas.waller@stud.th-rosenheim.de>
2023-12-14 01:02:25 +0100

    Added metrics counters

--- src/main/java/cc2023/teamrandom/ccservice/controller/TroetController.java
+++ src/main/java/cc2023/teamrandom/ccservice/controller/TroetController.java
@@ -7,6 +7,8 @@ import cc2023.teamrandom.ccservice.model.*;
 import cc2023.teamrandom.ccservice.model.gson.StatusSerializer;
 import cc2023.teamrandom.ccservice.model.gson.ZonedDateTimeTypeAdapter;
 import com.google.gson.GsonBuilder;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,10 +29,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 public class TroetController {
     public GetHomeService service = new GetHomeService();
-    private TroetListService troetListService;
 
 
-    Gson gson;
+	Gson gson;
 
     {
         gson = new GsonBuilder().registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeTypeAdapter()).registerTypeAdapter(MastodonStatus.class, new StatusSerializer())
@@ -39,11 +40,21 @@ public class TroetController {
 
     @Autowired
     private Logger logger;
-
-    public TroetController(TroetListService troetListService, Logger logger) {
-
-        this.troetListService = troetListService;
-        this.logger = logger;
+    private final Counter troetsrouteAccessCounter;
+    private final Counter rebloggedAccesCounter;
+    private final Counter getHomeAccessCounter;
+    @Autowired
+    public TroetController(MeterRegistry meterRegistry, Logger logger) {
+        this.troetsrouteAccessCounter = Counter.builder("custom.route.troetaccess")
+                .description("Counts the number of times a route is accessed")
+                .register(meterRegistry);
+        this.rebloggedAccesCounter = Counter.builder("custom.route.rebloggedaccess")
+                .description("Counts the number of times a route is accessed")
+                .register(meterRegistry);
+        this.getHomeAccessCounter = Counter.builder("custom.route.gethomeaccess")
+                .description("Counts the number of times a route is accessed")
+                .register(meterRegistry);
+		this.logger = logger;
     }
 
 
@@ -51,6 +62,7 @@ public class TroetController {
     public ResponseEntity<String> troets(@RequestParam(name = "limit", required = false) Integer limit,
                                          @RequestParam(name = "offset", required = false) Integer offset,
                                          @RequestParam(name = "troeter", required = false) String troeter) {
+        troetsrouteAccessCounter.increment();
         MastodonStatus[] entireMastodonStatuses = getHome().getBody();
         if(entireMastodonStatuses == null) return new ResponseEntity<>("500", HttpStatus.INTERNAL_SERVER_ERROR);
 
@@ -67,6 +79,7 @@ public class TroetController {
 
     @RequestMapping(value = "/gethome", method = GET)
     public ResponseEntity<MastodonStatus[]> getHome () {
+        getHomeAccessCounter.increment();
         MastodonStatus[] response = gson.fromJson(service.getHome(), MastodonStatus[].class);
         return new ResponseEntity<>(response, HttpStatus.OK);
 
@@ -74,7 +87,7 @@ public class TroetController {
 
     @RequestMapping(value = "/api/home/troets/reblogged")
     public ResponseEntity<String> reblogged(@RequestParam(name="troeter", required = false) String troeter){
-
+        rebloggedAccesCounter.increment();
         MastodonStatus[] entireMastodonStatuses = getHome().getBody();
         if(entireMastodonStatuses == null) return new ResponseEntity<>("404", HttpStatus.NOT_FOUND);
         ArrayList<MastodonStatus> resultAsArrayList = new ArrayList<>();
